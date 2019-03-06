797a77d3151c55c25fcb80e4c73a68d219c1bebc
Meni Samet <meni432@gmail.com>
2019-03-06 22:13:44 +0200

    tests

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const server = express()
 
 app.prepare()
 .then(() => {
-  const server = express()
   server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -34,3 +34,5 @@ app.prepare()
   console.error(ex.stack)
   process.exit(1)
 })
+
+module.exports = server
